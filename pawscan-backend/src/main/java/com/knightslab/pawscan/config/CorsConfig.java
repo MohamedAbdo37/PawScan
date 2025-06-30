bcf209dff803a5a518a944f5d3d412fe4e6d1cd4
MohamedAbdo37 <mabdelmoneim737@gmail.com>
2025-06-30 21:40:56 +0300

    link  firebase

---
+++ pawscan-backend/src/main/java/com/knightslab/pawscan/config/CorsConfig.java
@@ -0,0 +1,24 @@
+package com.knightslab.pawscan.config;
+
+import org.jetbrains.annotations.NotNull;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+public class CorsConfig {
+    
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(@NotNull CorsRegistry registry) {
+                registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+            }
+        };
+    }
+}
---
+++ pawscan-backend/src/test/java/com/knightslab/pawscan/PawScanBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knightslab.pawscan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawScanBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
