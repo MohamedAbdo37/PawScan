6f73bedf92079fd419bdf1a5bd57dc5a6ce2debe
MohamedAbdo37 <mabdelmoneim737@gmail.com>
2025-07-02 18:36:20 +0300

    fix a bug

--- pawscan-backend/src/main/java/com/knightslab/pawscan/config/FirebaseConfig.java
+++ pawscan-backend/src/main/java/com/knightslab/pawscan/config/FirebaseConfig.java
@@ -10,27 +10,31 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import java.io.FileInputStream;
+import java.io.IOException;
 
 @Configuration
 public class FirebaseConfig {
 
     @Value("${FIREBASE_CREDENTIAL_PATH}")
-    private String path;
+    private String firebaseCredentialPath;
 
     @Bean
-    public Firestore firestore() throws Exception {
+    public Firestore firestore() throws IOException {
+        System.out.println("FIREBASE_CREDENTIAL_PATH = " + firebaseCredentialPath);
+        FileInputStream serviceAccount = new FileInputStream(firebaseCredentialPath);
+
+        FirebaseOptions options = FirebaseOptions.builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
         if (FirebaseApp.getApps().isEmpty()) {
-            FileInputStream serviceAccount =
-                    new FileInputStream(this.path);
-
-            FirebaseOptions options = FirebaseOptions.builder()
-                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .build();
-
             FirebaseApp.initializeApp(options);
         }
+
         System.out.println("✅ Firebase has been initialized.");
+
         return FirestoreClient.getFirestore();
     }
+
 }
 
