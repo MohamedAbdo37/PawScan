d02e5cc903ec960607c8de297a9e1c495f12fd1e
MohamedAbdo37 <mabdelmoneim737@gmail.com>
2025-07-02 18:54:02 +0300

    fix a rout bug

--- pawscan-backend/src/main/java/com/knightslab/pawscan/HomeController.java
+++ pawscan-backend/src/main/java/com/knightslab/pawscan/HomeController.java
@@ -6,9 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-    @RequestMapping(value = "/{[path:[^\\\\.]*}")
+    @RequestMapping(value = "/{path:[^\\.]*}")
     public String redirect() {
         return "forward:/";
     }
-
 }
+
