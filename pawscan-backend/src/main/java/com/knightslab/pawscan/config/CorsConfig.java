e57b4b63e74df3aee5849bb94996d2d536fd8383
MohamedAbdo37 <mabdelmoneim737@gmail.com>
2025-07-02 22:59:31 +0300

    add Annotation

--- pawscan-backend/src/main/java/com/knightslab/pawscan/config/CorsConfig.java
+++ pawscan-backend/src/main/java/com/knightslab/pawscan/config/CorsConfig.java
@@ -2,9 +2,11 @@ package com.knightslab.pawscan.config;
 
 import org.jetbrains.annotations.NotNull;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
 public class CorsConfig {
     
 
